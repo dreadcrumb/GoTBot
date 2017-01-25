aa32803a0c50185f217afbf5650f8f01379d3d85
Leander Suda <Leander Suda>
2017-01-25 20:22:57 +0100

    not f working

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from flask import Flask
 from flask import request
 from flask import make_response
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -18,12 +19,13 @@ def hello_name(name):
 @app.route("/webhook", methods=['POST'])
 def webhook():
 	
-	
 	req = request.get_json(silent=True, force=True)
-	
+
 	parameters = req.get("result").get("parameters")
 	
 	character = parameters["character"]
+
+	character = "Jon Snow"
 	
 	character = character.title()
 	if " " in character:
@@ -32,34 +34,30 @@ def webhook():
 
 	content = urlopen('http://gameofthrones.wikia.com/wiki/' + character).read()
 	soup = BeautifulSoup(content, "html.parser")
-
 	isPage = soup.find('aside', attrs={"class":"portable-infobox"})
+	#isNoArticle = soup.find('div', attrs={"class":"noarticletext"})
 
-	isNoArticle = soup.find('div', attrs={"class":"noarticletext"})
+	res = None
 
-	if isNoArticle != None:
-		newLink = soup.find('a', attrs={"href"})
-		print(newLink)
-
+	#if isNoArticle != None:
+	#	newLink = soup.find('a', attrs={"href"})
+	#	print(newLink)
 	if isPage == None:
 		print("Name is unclear, try again")
 	else:
 		status =soup.find(text='Alive')
-
 		if status == None:
 			print(character + " is dead")	
 			res = False
 		else:
 			print(character + " is alive")
-			alive = True
-			
-	#deathEpsiode = soup.find('div'
-	
+			res = True
+		
+
+
 	r = make_response(res)
 	print(r)
-			
 	return r
-
 			
 if __name__ == "__main__":
     app.run()
